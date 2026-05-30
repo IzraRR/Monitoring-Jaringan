4eb7272053531bb47baa57f6e9feb0ef649aae61
IzraRR <wsdhij45@gmail.com>
2026-05-30 20:40:36 +0700

    Configure logo.png as the favicon for the app layout and login pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Monitoring SMKN 53')</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
     
     <link href="{{ asset('vendor/css/bootstrap.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('vendor/css/bootstrap-icons.css') }}">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Admin Jaringan SMKN 53</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
     <link href="{{ asset('vendor/css/bootstrap.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('vendor/css/bootstrap-icons.css') }}">
     <style>
